134a7d71435a559439b3cb50fee0e051a7ff2678
Marco <marco.gzz1405@gmail.com>
2020-02-09 21:07:27 -0600

    add apiservice

--- app/src/main/java/com/example/liverpool/api/ApiService.java
+++ app/src/main/java/com/example/liverpool/api/ApiService.java
@@ -1,4 +1,18 @@
 package com.example.liverpool.api;
 
-public class ApiService {
+import com.example.liverpool.models.Productos;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiService {
+
+    @GET("tienda")
+    @FormUrlEncoded
+    Call<Productos> getProductos(@Field("s") String producto,
+                            @Field("d3106047a194921c01969dfdec083925") String json);
+
 }
--- app/src/main/java/com/example/liverpool/api/PresenterBusqueda.java
+++ app/src/main/java/com/example/liverpool/api/PresenterBusqueda.java
@@ -10,19 +10,46 @@ import retrofit2.Retrofit;
 
 public class PresenterBusqueda implements Callback<Productos> {
 
+    public ListenerBusqueda listener;
+
     public void attempBusqueda(String s){
         Retrofit retrofit = Utils.getClient();
         ApiService apiService = retrofit.create(ApiService.class);
-        Call<Productos> request = apiService
+        Call<Productos> request = apiService.getProductos(s, "json");
+        request.enqueue(this);
+        if(listener != null){
+            listener.showProgress();
+        }
     }
 
     @Override
     public void onResponse(Call<Productos> call, Response<Productos> response) {
-
+        if(response.isSuccessful()){
+            Productos productos = response.body();
+            if(productos != null) {
+                if(listener != null) {
+                    listener.successEntry(productos);
+                }
+            } else {
+                if(listener != null){
+                    listener.showErrorMessage("Successful is failed");
+                }
+            }
+        } else {
+            if(listener != null){
+                listener.showErrorMessage("Error");
+            }
+        }
+        if(listener != null){
+            listener.hideProgress();
+        }
     }
 
     @Override
     public void onFailure(Call<Productos> call, Throwable t) {
-
+        if(listener != null){
+            listener.hideProgress();
+            listener.showErrorMessage(t.getMessage());
+        }
     }
 }
